6f685851d7c5569fc8b5be8767c33da47102a342
actionanand <meetanandns@gmail.com>
2019-12-25 20:11:19 +0530

    add note works

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 const success = chalk.bold.green;
 
-const msg = getNotes();
-// console.log(msg);
 // console.log(success('Success!'));
 // console.log(process.argv);
 // console.log(yargs.argv);
@@ -30,8 +28,7 @@ yargs.command({
       }
     },
     handler: function (argv) {
-        console.log('Title: ' +argv.title);
-        console.log('Body: ' +argv.body);
+      notes.addNote(argv.title, argv.body)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,5 +1,39 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+const success = chalk.bold.green;
+
 const getNotes = function() {
     return 'your notes...'
 }
 
-module.exports = getNotes
+const addNote = function(title, body) {
+    const notes = loadNotes();
+    notes.push({
+        title: title,
+        body: body
+    });
+    saveNotes(notes);
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+    } catch(err) {
+        console.log('Created new ' + success.inverse('notes.json') + ' file!');
+        return [];
+    }
+}
+
+const saveNotes = function(notes) {
+    const jsonData = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', jsonData);
+    console.log(success('Note add successfully!'))
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
